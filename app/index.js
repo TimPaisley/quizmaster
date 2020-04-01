185705383ee4196f733cd96ef2b9ab3f4e2d311c
Tim Paisley <tepaisley@gmail.com>
2020-04-01 20:38:32 +1300

    Ignore unnecessary words and spaces in answer

--- app/index.js
+++ app/index.js
@@ -11,43 +11,45 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === config.prefix + 'start') {
+  if (message.content === config.prefix + 'start') {
     message.channel.send("Starting trivia! Type `?stop` to stop.");
 
     getQuestion(message.channel);
     const interval = setInterval(
-      function() { getQuestion(message.channel) },
+      function () { getQuestion(message.channel) },
       config.interval_in_seconds * 1000
     );
 
     activeChannels[message.channel.id] = interval;
-    console.log (`Starting trivia on channel #${message.channel.id}`);
+    console.log(`Starting trivia on channel #${message.channel.id}`);
   }
-  
+
   else if (message.content === config.prefix + 'stop') {
     if (activeChannels[message.channel.id]) {
       message.channel.send("Stopping trivia. Type `?start` to start again.");
 
       clearInterval(activeChannels[message.channel.id]);
       activeChannels[message.channel.id] = null;
-      console.log (`Stopping trivia on channel #${message.channel.id}`);
+      console.log(`Stopping trivia on channel #${message.channel.id}`);
     } else {
       message.channel.send("I'm not running yet! Type `?start` to start.");
     }
-	}
+  }
 });
 
-function getQuestion (channel) {
+function getQuestion(channel) {
   const random = Math.floor(Math.random() * questions.length)
   var question = questions[random];
 
   sendQuestion(channel, question);
-  // console.log ("Asking question: ", question);
+  console.log("Asking question: ", question);
 }
 
-function sendQuestion (channel, question) {
+function sendQuestion(channel, question) {
   const filter = response => {
-    return response.content.toLowerCase().includes(question.answer.toLowerCase());
+    const guess = response.content.toLowerCase();
+    const answer = cleanAnswer(question.answer);
+    return guess.includes(answer);
   };
 
   channel.send(formatQuestion(question)).then(() => {
@@ -65,7 +67,19 @@ function sendQuestion (channel, question) {
   });
 }
 
-function formatQuestion (question) {
+function cleanAnswer(answer) {
+  var words = ["of", "the", "in", "on", "at", "to", "a", "is", "their", "was", "and"];
+  var wordRegex = new RegExp('\\b(' + words.join('|') + ')\\b', 'g');
+  var charRegex = new RegExp('[^0-9a-zA-Z ]+', 'g');
+  var spaceRegex = new RegExp('  +', 'g');
+  return (answer || '')
+    .replace(wordRegex, '')
+    .replace(charRegex, '')
+    .replace(spaceRegex, ' ')
+    .toLowerCase().trim();
+}
+
+function formatQuestion(question) {
   return `
 -----
 Category: *${question.category} (${question.value})*
@@ -74,13 +88,13 @@ Category: *${question.category} (${question.value})*
 `
 }
 
-function formatCorrectAnswer (author, answer) {
+function formatCorrectAnswer(author, answer) {
   return `
 :partying_face: ${author} got the correct answer! The answer was *${answer}*.
 `
 }
 
-function formatNoAnswer (answer) {
+function formatNoAnswer(answer) {
   return `
 :confounded: Nobody was correct! The answer was *${answer}*.
 `
